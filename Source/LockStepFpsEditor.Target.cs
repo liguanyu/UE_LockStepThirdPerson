d41786fd97acf85cf7ab5e4bf08d122babcf4d3f
guany <guany@local>
2026-03-04 02:38:02 +0800

    修复ue5.7编译错误 BuildSettingsVersion V6

--- Source/LockStepFpsEditor.Target.cs
+++ Source/LockStepFpsEditor.Target.cs
@@ -6,7 +6,7 @@ public class LockStepFpsEditorTarget : TargetRules
     public LockStepFpsEditorTarget(TargetInfo Target) : base(Target)
     {
         Type = TargetType.Editor;
-        DefaultBuildSettings = BuildSettingsVersion.V5;
+        DefaultBuildSettings = BuildSettingsVersion.V6;
         IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
         ExtraModuleNames.Add("LockStepFps");
     }
